f78bc94cecb79b7d6e92f7cb483d7884b2946d6c
Ryan Goodwin <goodwinryan2@gmail.com>
2024-10-08 23:05:28 -0400

    Update plugin to actually register on the site

--- plugins/grandma2/export.js
+++ plugins/grandma2/export.js
@@ -1,57 +1,38 @@
-import sanitize from 'sanitize-filename';
-import xmlbuilder from "xmlbuilder";
+import xmlbuilder from 'xmlbuilder';
+import Fixture from '../../lib/model/Fixture.js';
+import Mode from '../../lib/model/Mode.js';
 
-import Fixture from '../../lib/model/Fixture';
-import Mode from '../../lib/model/Mode';
 
-export const version = `0.1.0`; // semantic versioning of export plugin
+export const version = `0.1.0`;
 
 /**
  * @param {Fixture[]} fixtures An array of Fixture objects, see our fixture model
- * @param {object} options Some global options, for example:
- * @param {string} options.baseDirectory Absolute path to OFL's root directory
- * @param {Date} options.date The current time.
- * @param {string | undefined} options.displayedPluginVersion Replacement for plugin version if the plugin version is used in export.
- * @returns {Promise<*[]>} All generated files (see file schema above)
+ * @param {object} options Some global options
+ * @returns {Promise<object[], Error>} All generated files (see file schema above)
  */
 export async function exportFixtures(fixtures, options) {
-  const outFiles = await Promise.all(fixtures.map(async fixture => {
-    try {
-      return await getFixtureFiles(fixture, options);
-    }
-    catch (error) {
-      throw new Error(`Exporting fixture ${fixture.manufacturer.key}/${fixture.key} failed: ${error}`, {
-        cause: error,
-      });
-    }
-  }));
+  const outFiles = []
 
-  return outFiles
-}
+  fixtures.map(fixture => {
+    fixture.modes.map(mode => {
+      outFiles.push(generateFile(fixture, mode, options));
+    });
+  });
 
-async function getFixtureFiles(fixture, options) {
-  const modeFiles = await Promise.all(fixture.modes.map(mode => {
-    try {
-      return getFixtureModeFile(fixture, mode, options);
-    } catch (err) {
-      throw new Error(`Exporting fixture ${fixture.manufacturer.key}/${fixture.key}/${mode.name} mode failed: ${error}`, {
-        cause: error,
-      });
-    }
-  }));
-  return modeFiles;
+  return outFiles;
 }
 
 /**
- * @param {Fixture} fixture The fixure to export
- * @param {Mode} mode fixture mode
+ * Generate fixture file to be downloaded
+ * @param {Fixture} fixture the fixture to download
+ * @param {Mode} mode the fixtures mode
  * @param {object} options Global options
- * @returns {Promise<object>} The generated fixture file
+ * @returns {object} the file that will be downloaded
  */
-async function getFixtureModeFile(fixture, mode, options) {
-  const xml = xmlbuilder.begin()
+function generateFile(fixture, mode, options) {
+  let xml = xmlbuilder.begin()
     .declaration('1.0', 'UTF-8')
-    .element({
+    .ele({
       MA: {
         "@major_vers": "3",
         "@minor_vers": "2",
@@ -90,53 +71,12 @@ async function getFixtureModeFile(fixture, mode, options) {
         }
       }
     });
-
-  // getChannelTypes(xml, mode)
-
-  const sanitizedFileName = sanitize(`${fixture.manufacturer.name}@${fixture.name}@${mode.shortName}.xml`).replaceAll(/\s+/g, `-`);
-
-  return await {
-    name: `${sanitizedFileName}`,
+  return {
+    name: `${fixture.manufacturer.key}@${fixture.key}@${mode.shortName}.xml`,
     content: xml.end({
       pretty: true,
       indent: '  '
     }),
-    mimetype: 'application/xml'
-  };
-
-}
-
-/**
- * @param {Mode} mode the current mode
- * @returns {object} the list of channels for that fixture in that mode
- */
-function getChannelTypes(xml, mode) {
-  return mode.channels.reduce((acc, channel, idx) => {
-    return {
-      ...acc,
-      ChannelType: {
-        "@index": `${idx}`,
-        "@attribute": "",
-        "@feature": "",
-        "@preset": "",
-        "@course": "",
-        "@fine": "",
-        "@default": "",
-        ChannelFunction: {
-          "@index": "0",
-          "@from": "",
-          "@to": "",
-          "@min_dmx_24": "",
-          "@max_dmx_24": "",
-          "@physfrom": "",
-          "@physto": "",
-          "@subattribute": "",
-          "@attribute": "",
-          "@feature": "",
-          "@preset": "",
-          // TODO Chanel Sets
-        }
-      }
-    }
-  });
+    mimetype: `application/xml`,
+  }
 }
